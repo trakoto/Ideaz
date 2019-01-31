7b142e94a7d33db7ba5b3a70375c9b1a7f2b9718
act1022 <act1022@atl42act080821.daugherty.com>
2019-01-31 12:47:57 -0500

    First commit

---
+++ src/main/java/com/trakoto/Ideaz/IdeazApplication.java
@@ -0,0 +1,15 @@
+package com.trakoto.Ideaz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IdeazApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(IdeazApplication.class, args);
+	}
+
+}
+
